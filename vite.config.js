1a86e88b96dcb1c016d24c7b2d5f77b73cf98a44
solithcy <me@rose.gay>
2025-07-29 16:13:19 +0100

    make project work w/ vite

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import {defineConfig} from "vite";
+import {resolve} from "path";
+
+export default defineConfig({
+  root: "src",
+  publicDir: "../static",
+  build: {
+    rollupOptions: {
+      input: {
+        login: resolve(__dirname, "src/login.html"),
+        register: resolve(__dirname, "src/register.html"),
+        caseAnalytics: resolve(__dirname, "src/case-analytics.html"),
+        createCase: resolve(__dirname, "src/create-case.html"),
+        gamePlaying: resolve(__dirname, "src/game-playing.html"),
+        studentHomepage: resolve(__dirname, "src/student-homepage.html"),
+        teacherDashboard: resolve(__dirname, "src/teacher-dashboard.html"),
+        unauthorized: resolve(__dirname, "src/unauthorised.html"),
+      }
+    },
+    outDir: resolve(__dirname, "dist")
+  }
+});
