29d2ad40b1f1f31384a1addc876e524360331937
solithcy <me@rose.gay>
2025-07-30 14:45:26 +0100

    initial analytics work

--- src/assets/js/const.js
+++ src/assets/js/const.js
@@ -1,1 +1,16 @@
 export const API_URL = 'http://localhost:3000/api';
+
+export const baseChartOpts = {
+  color: {
+    pairing: {
+      option: 2
+    }
+  },
+  toolbar: {
+    enabled: false
+  }
+}
+
+export const baseMeterChartOpts = {
+  ...baseChartOpts,
+}
